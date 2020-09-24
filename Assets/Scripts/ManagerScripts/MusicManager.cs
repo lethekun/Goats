4b7dea8f02ed74219405857269579ec846865755
Emrah Uluçay <ulucayemrah@gmail.com>
2020-09-24 22:48:50 +0300

    Hayırlısı... Commit'i

---
+++ Assets/Scripts/ManagerScripts/MusicManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager _instance;
+    public static MusicManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.LogError("MusicManager Yok?");
+            }
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance)
+            DestroyImmediate(gameObject);
+        else
+            _instance = this;
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
