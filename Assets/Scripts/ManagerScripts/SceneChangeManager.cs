db99b30a18dffc7ef094a0ccd8bdc89fdc362698
Emrah Uluçay <ulucayemrah@gmail.com>
2020-09-27 23:51:05 +0300

    Tiny Sauce ayarları

--- Assets/Scripts/ManagerScripts/SceneChangeManager.cs
+++ Assets/Scripts/ManagerScripts/SceneChangeManager.cs
@@ -5,14 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangeManager : MonoBehaviour
 {
+    private void Start()
+    {
+        TinySauce.OnGameStarted(SceneManager.GetActiveScene().buildIndex.ToString());
+    }
     public void LoadNextScene()
     {
+        TinySauce.OnGameFinished(SceneManager.GetActiveScene().buildIndex.ToString(), true, 0);
         Debug.Log("LoadNextScene clicked!");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void RestartScene()
     {
+        TinySauce.OnGameFinished(SceneManager.GetActiveScene().buildIndex.ToString(), true, 0);
         Debug.Log("RestardScene clicked!");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
